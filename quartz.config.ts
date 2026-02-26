2424d5760ddb4757c26e5be78d556cf8551f86c9
Gata-chan <84407182+Gata-chan@users.noreply.github.com>
2026-02-26 17:35:22 +0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gata-chan.github.io/GURPS-Traveller",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
